408df863dc7d60693903864d1b116c764111beba
Themba Mtshelwane <Thembamm3@gmail.com>
2025-11-20 14:52:11 +0200

    Add JobListings component and update App.vue to include it; create jobs.json for job data

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import Hero from "./components/Hero.vue";
 import HomeCards from "./components/HomeCards.vue";
 import Navbar from "./components/Navbar.vue";
+import JobListings from "./components/JobListings.vue";
 </script>
 
 <template>
   <Navbar />
   <Hero />
   <HomeCards />
+  <JobListings />
 </template>
