b547193f0ebfeecf71f11f2362cd147522ab5d9e
Themba Mtshelwane <Thembamm3@gmail.com>
2025-11-20 18:20:35 +0200

    Add NotFoundView component and configure routing for 404 error handling

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/view/HomeView.vue";
 import path from "path";
 import JobsView from "@/view/JobsView.vue";
+import NotFoundView from "@/view/NotFoundView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       path: "/jobs",
       name: "jobs",
       component: JobsView,
+    },
+    {
+      path: "/:catchAll(.*)",
+      name: "NotFound",
+      component: NotFoundView,
     },
   ],
 });
