6f18182b5948bd2060a131aab18fefe7051a9dad
Themba Mtshelwane <Thembamm3@gmail.com>
2025-11-20 17:45:53 +0200

    Add JobsView component and configure routing for job listings

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/view/HomeView.vue";
+import path from "path";
+import JobsView from "@/view/JobsView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+    {
+      path: "/jobs",
+      name: "jobs",
+      component: JobsView,
     },
   ],
 });
